89df6c07fef25886b060b983c64c21ecc9b93489
DanijelaMilanovic <danijelamilanovic222@gmail.com>
2022-09-11 20:52:26 +0200

    Korpa

--- Routes.php
+++ Routes.php
@@ -6,6 +6,8 @@
         App\Core\Route::post('|^user/login/?$|',                    'Main',                   'postLogin'),
         App\Core\Route::get('|^user/logout/?$|',                    'Main',                   'getLogout'),
         App\Core\Route::get('|^book/([0-9]+)/?$|',                  'Book',                   'show'),
+        App\Core\Route::get('|^cart/?$|',                           'Cart',                   'show'),
+
 
         # API rute:
         App\Core\Route::get('|^api/title/([0-9]+)/?$|',             'ApiTitle',               'getTitleByCategoryId'),
@@ -15,7 +17,7 @@
         App\Core\Route::get('|^api/bookmarks/clear/?$|',            'ApiBookmark',            'clear'),
 
         # User role routes:
-        App\Core\Route::get('|^user/profile/?$|',                   'UserDashboard',          'index'),
+        App\Core\Route::get('|^user/profile/?$|',                   'UserDashboard',           'index'),
         App\Core\Route::get('|^user/addBook/?$|',                   'UserBook',                'getAddBook'),
         App\Core\Route::post('|^user/addBook/?$|',                  'UserBook',                'postAddBook'),
 
